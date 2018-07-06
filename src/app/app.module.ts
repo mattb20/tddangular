adb5774723868350a7592f64b61f115ec31cc43b
mattb20 <matt_burgoyne@live.co.uk>
2018-07-06 09:11:15 +0100

    moved books folder into newly created components folder, created models folder inside app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [
